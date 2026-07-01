303441d6064b615e0292710ff1c2df51bd78a466
ʌzvkʌ ~ <jezebel.assad@outlook.com>
2026-07-01 09:09:55 -0300

    feat: route /categories linked to index()

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/categories', []);
+Route::get('/categories', [CategoryController::class, 'index']);
